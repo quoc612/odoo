1f5253d312fe605268cf844ed3cedc6df138fdb7
Tran Dan ver 2 <quoc.nguyenvan@vti.com.vn>
2022-05-18 14:25:52 +0700

    fixed

--- my_project/mybook/models/return_book.py
+++ my_project/mybook/models/return_book.py
@@ -4,11 +4,10 @@ from odoo.exceptions import ValidationError
 
 class ReturnBook(models.Model):
     _name = "book.return"
-    _inherit = "book.info"
     book_borrow_id = fields.Many2one('book.borrow', string="Book borrow ID")
     name = fields.Char(string="Tên sinh viên")
     name_of_leader = fields.Char(string="Người quản lý")
-    borrow_book_list = fields.Char(string="Các cuốn sách trả")
+    name_of_book = fields.Char(string="Các cuốn sách mượn")
     date_borrow_book = fields.Date(string="Ngày trả sách")
     total_late_book = fields.Float(string="Tổng tiền trả muộn")
 
@@ -17,8 +16,4 @@ class ReturnBook(models.Model):
         self.name = self.book_borrow_id.name
         self.name_of_leader = self.book_borrow_id.name_of_leader
         self.date_borrow_book = self.book_borrow_id.date_borrow_book
-        self.borrow_book_list = self.book_borrow_id.borrow_book_list
-
-    # @api.onchange("total_late_book")
-    # def total_late(self):
-    #     self.total_late_book = self.
+        self.name_of_book = self.book_borrow_id.name_of_book
